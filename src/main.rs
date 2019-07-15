39a1fc0cb8ee3fdba97f779ee117392427bdc45c
Brandon Danielski <bld@pdx.edu>
2019-07-15 14:01:01 -0700

    Disclaimer and references.txt addition

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod running_alg;
 
 fn main() {
 
-  //This line was found at: https://stackoverflow.com/questions/34837011/how-to-clear-terminal-screen-in-rust-after-new-line-is-printing/34837038
-  print!("{}[2J", 27 as char);
+  print!("{}[2J", 27 as char);//Ref. 1
+  println!("DISCLAIMER!!!!!!!!: Throughout the code there will be references to code that I used throughout the project that was not my own however they will not be listed in a traditional way.  Instead if you see a comment that has a 'Ref. #' this is a reference to a link in my references.txt page in my primary directory for tis project.  This will allow for cleaner and more readable code.  If you would like to see each reference link please go the to references.txt page.  Now back to the game. \n\n");
   menu::main_menu(); 
 }
--- src/menu.rs
+++ src/menu.rs
@@ -2,18 +2,18 @@ use std::io::{stdin,stdout,Write};
 use std::process::exit;
 
 fn create_random_maze(){
-  print!("{}[2J", 27 as char);
+  print!("{}[2J", 27 as char);//Ref. 1
   maze_solving_strategy();
 }
 
 fn load_in_maze(){
-  print!("{}[2J", 27 as char);
+  print!("{}[2J", 27 as char);//Ref. 1
   maze_solving_strategy();
 
 }
 
 fn maze_solving_strategy(){
-  print!("{}[2J", 27 as char);
+  print!("{}[2J", 27 as char);//Ref. 1
   println!("We made it this far!");
   //1. Right hand rule.
   //2. I'll solve it myself.
@@ -23,7 +23,7 @@ pub fn main_menu() {
 	println!("Welcome to the Maze Running Simulator. \n Please Choose from the following options: \n 1. Run Randomly Generated Maze \n 2. Load in Maze to Run \n 3. Help \n 4. Exit");
 
 
-    //This block was found here: https://users.rust-lang.org/t/how-to-get-user-input/5176/2 by user: jethrogb
+    //Ref. 2 begin
     let mut s=String::new();
     let _=stdout().flush();
     stdin().read_line(&mut s).expect("Did not enter a correct string");
@@ -33,6 +33,7 @@ pub fn main_menu() {
     if let Some('\r')=s.chars().next_back() {
         s.pop();
     }
+    //Ref. 2 end
     if s == "1" {
 	create_random_maze();	
     }
@@ -40,12 +41,12 @@ pub fn main_menu() {
 	load_in_maze();
     }
     else if s == "3"{
-  	print!("{}[2J", 27 as char);
+  	print!("{}[2J", 27 as char);//Ref. 1
 	println!("Help coming soon...");
 	return main_menu();
     }
     else if s == "4"{
-  	print!("{}[2J", 27 as char);
+  	print!("{}[2J", 27 as char);//Ref. 1
         println!("Bye-bye, come back soon!");
         exit(0);
     }
